9f5a6ff907c4c4b345e43b6aa312332e89c18fd3
dylan <justbeawesome9@gmail.com>
2024-01-30 00:29:49 -0500

    feat: scene and perspective / orthographic camera

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,5 @@
+import * as THREE from "three"
+
 import Experience from "./experience.js";
 
 export default class Camera {
@@ -6,6 +8,44 @@ export default class Camera {
         this.sizes = this.experience.sizes;
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
-        console.log(this.experience)
+
+        this.createPerspectiveCamera();
+        console.log(this)
+    }
+
+    createPerspectiveCamera() {
+        this.perspectiveCamera = new THREE.PerspectiveCamera(
+            35,
+            this.sizes.aspect,
+            .1,
+            1000
+        )
+
+        this.scene.add(this.perspectiveCamera)
+    }
+
+    createOrthographicCamera() {
+        this.frustrum = 5
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.sizes.frustrum) / 2,
+            (this.sizes.aspect * this.sizes.frustrum) / 2,
+            this.sizes.frustrum / 2,
+            -this.sizes.frustrum / 2,
+            -100,
+            100
+        )
+
+        this.scene.add(this.orthographicCamera)
+    }
+
+    resize() {
+        this.perspectiveCamera.aspect = this.sizes.aspect;
+        this.perspectiveCamera.updateProjectionMatrix()
+
+        this.orthographicCamera.left = (-this.sizes.aspect * this.sizes.frustrum) / 2
+        this.orthographicCamera.right = (this.sizes.aspect * this.sizes.frustrum) / 2
+        this.orthographicCamera.top = this.sizes.frustrum / 2
+        this.orthographicCamera.bottom = -this.sizes.frustrum / 2
+        this.orthographicCamera.updateProjectionMatrix();
     }
 }
