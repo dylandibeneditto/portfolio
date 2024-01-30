0c9dfb31e277acb7bce38ab86e16259575aff8a9
dylan <justbeawesome9@gmail.com>
2024-01-30 00:36:19 -0500

    feat: renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,5 +1,39 @@
+import * as THREE from "three"
+
+import Experience from "./experience";
+
 export default class Renderer {
-    constructor(canvas) {
+    constructor() {
+        this.experience = new Experience();
+        this.sizes = this.experience.sizes;
+        this.scene = this.experience.scene;
+        this.canvas = this.experience.canvas;
+        this.camera = this.experience.camera;
+
+        this.setRenderer();
+    }
+
+    setRenderer() {
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: this.canvas,
+            antialias: true
+        })
         
+
+        this.renderer.physicallyCorrectLights = true;
+        this.renderer.toneMapping = THREE.CineonToneMapping;
+        this.renderer.toneMappingExposure = 1.75;
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this.renderer.setSize(this.sizes.width, this.sizes.height);
+        this.renderer.setPixelRatio(this.sizes.pixelRatio)
+    }
+
+    resize() {
+
+    }
+
+    update() {
+        this.renderer.render(this.scene, this.camera.perspectiveCamera)
     }
 }
