866b8050ba2ecb3cd82aa854478f17e31f951eb2
dylan <justbeawesome9@gmail.com>
2024-01-30 19:48:35 -0500

    feat: remove three boilerplate and dependency

--- js/experience.js
+++ js/experience.js
@@ -1,43 +1,10 @@
-import * as THREE from "three"
-
-import Sizes from "./utils/sizes.js";
-import Time from "./utils/time.js"
-
-import Camera from "./camera.js";
-import Renderer from "./renderer.js";
-
 export default class Experience {
     static instance;
-    constructor(canvas) {
+    constructor() {
         if (Experience.instance) {
             return Experience.instance
         }
         Experience.instance = this;
-
-        this.canvas = canvas;
-        this.scene = new THREE.Scene();
-        this.time = new Time()
-        this.sizes = new Sizes();
-        this.camera = new Camera();
-        this.renderer = new Renderer(this.canvas);
-
-        this.time.on("update", ()=> {
-            this.update()
-        })
-
-        this.sizes.on("resize", ()=> {
-            this.resize();
-        })
-    }
-    
-    resize() {
-        this.renderer.resize();
-        this.camera.resize();
-    }
-
-    update() {
-        this.camera.update();
-        this.renderer.update();
     }
 
 }
