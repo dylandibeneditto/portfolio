da32401fcd027b5a44049c8d336d8b40a1cbb9cc
dylan <justbeawesome9@gmail.com>
2024-01-31 20:04:00 -0500

    feat: reimplemented three.js and boilerplate + scene.js

---
+++ js/scene.js
@@ -0,0 +1,8 @@
+import * as THREE from "three"
+import Experience from "./experience.js"
+
+export default class Scene {
+    constructor() {
+        
+    }
+}
---
+++ js/utils/sizes.js
@@ -0,0 +1,19 @@
+import { EventEmitter } from "events";
+
+export default class Sizes extends EventEmitter {
+    constructor() {
+        super()
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.aspect = this.width/this.height;
+        this.pixelRation = Math.min(window.devicePixelRation, 2)
+
+        window.addEventListener("resize", ()=> {
+            this.width = window.innerWidth;
+            this.height = window.innerHeight;
+            this.aspect = this.width/this.height;
+            this.pixelRation = Math.min(window.devicePixelRation, 2)
+            this.emit("resize")
+        })
+    }
+}
