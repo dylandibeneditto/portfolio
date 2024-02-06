cb2384196f6a2067aafffcf8378d2e5faf37dae5
dylan <justbeawesome9@gmail.com>
2024-02-05 23:28:26 -0500

    fix: formatting

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import "./css/utils.css"
 import "./css/styles.css";
 import Experience from "./js/experience.js";
 
-setTimeout(()=> {
+setTimeout(() => {
         console.log(`%cWhy is it that when one man builds a wall, the next man immediately needs to know what's on the other side?\n\t- George R.R. Martin`, " font-size: .8rem; color: white; padding: .5rem;")
 }, 0)
 
