eb07b50b10908690e2c516143b9c21e94910f02c
Dylan DiBeneditto <dibenedittod@gmail.com>
2024-08-31 16:22:04 -0400

    feat: complete sidebar layout and custom icons

--- components/sidebar label/sidebarlabel.tsx
+++ components/sidebar label/sidebarlabel.tsx
@@ -1,5 +1,23 @@
-export default function SidebarLabel(props: {text: string, icon: string}) {
+import AboutIcon from "../icons/about";
+import ProjectsIcon from "../icons/projects";
+import "./sidebarlabel.css";
+import Link from "next/link";
+
+export default function SidebarLabel(props: {text: string, icon: string, link: string, external: boolean}) {
     return (
-        <>{props.text}</>
+        <Link href={props.link} target={props.external ? "_blank" : ""}>
+        <div className="label">
+            <div className="icon">
+                {props.icon == "about" ? (
+                    <AboutIcon></AboutIcon>
+                 ) : props.icon == "projects" ? (
+                    <ProjectsIcon></ProjectsIcon>
+                 ) : (
+                    <span className="material-symbols-outlined">{props.icon}</span>
+                )}
+            </div>
+            <div className="title">{props.text}</div>
+        </div>
+        </Link>
     )
 }
