02282c7142b11a2bedab1bdcee1203e7810ff65f
Dylan DiBeneditto <dibenedittod@gmail.com>
2024-09-02 21:42:43 -0400

    test

--- components/icons/email.tsx
+++ components/icons/email.tsx
@@ -1,10 +1,6 @@
 export default function EmailIcon() {
   return (
-    <svg
-      viewBox="0 0 602 602"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg viewBox="0 0 602 602" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         fill-rule="evenodd"
         clip-rule="evenodd"
