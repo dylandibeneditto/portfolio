ddab5673779afd5de840625135c5a6974f79da4a
dylan <dibenedittod@gmail.com>
2026-02-04 21:03:12 -0500

    canvas setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { innerWidth, innerHeight } from "svelte/reactivity/window";
+</script>
+
+<canvas height={innerHeight.current} width={innerWidth.current}></canvas>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	canvas {
+		position: fixed;
+		inset: 0;
+		width: 100vw;
+		height: 100vh;
+		display: block; /* removes inline-element baseline gap */
+		z-index: -1;
+	}
+</style>
